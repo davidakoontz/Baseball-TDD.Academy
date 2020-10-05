d1dcb04e94af708b5d2839654a520147bed27bd1
davidakoontz <david@koontz.name>
2020-10-04 20:17:46 -0500

    Initial Commit

---
+++ Baseball-TDD/Baseball_TDDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Baseball_TDDApp.swift
+//  Baseball-TDD
+//
+//  Created by David on 10/4/20.
+//
+
+import SwiftUI
+
+@main
+struct Baseball_TDDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Baseball-TDD/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Baseball-TDD
+//
+//  Created by David on 10/4/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
