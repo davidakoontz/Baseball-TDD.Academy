9d1c5a237ad9aee42f07e81b358ab1af67ac5221
davidakoontz <david@koontz.name>
2020-12-13 20:16:29 -0600

    removed the Zeroth inning - just going to deal with the counting with Zero issue for now.

--- Baseball-Tests/InningTest.swift
+++ Baseball-Tests/InningTest.swift
@@ -12,6 +12,8 @@ class InningTest: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        // visitors always bat first (top of innings)
+
     }
 
     override func tearDownWithError() throws {
@@ -23,23 +25,54 @@ class InningTest: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func testInningHasNumber() throws {
-        XCTAssertTrue( Inning.exampleGame[1].number.contains("1") )
-        
-        XCTAssertTrue( Inning.exampleGame[9].number.contains("9") )
+    func testInit() throws {
+        let bob = Player(name: "Bob Swift", number: "3", position: "6", atBat: "K")
+        let larry = Player(name: "Larry Ellison", number: "2", position: "2", atBat: "K")
+    
+        let first = Inning(number: "1",
+                              top: [ bob ],
+                              bottom: [ larry],
+                              summary: "0 to 0")
+
+        XCTAssertTrue( first.number.contains("1") )
+        XCTAssertTrue( first.top[0].name.contains("Swift"))
+        XCTAssertTrue( first.bottom[0].name.contains("Ellison"))
+        XCTAssertTrue( first.summary.contains("0 to 0") )
+
     }
 
     func testInningHasSummary() throws {
-        XCTAssertTrue( Inning.exampleGame[0].summary.contains("0 to 0"))
+        let firstInning = Inning(number: "1",
+                              top: [],
+                              bottom: [],
+                              summary: "0 to 0")
+        XCTAssertTrue( firstInning.summary.contains("0 to 0"))
     }
     
-    func testInningOneHasScore_0_to_0() throws {
-        XCTAssertTrue( Inning.exampleGame[1].summary.contains("0 to 0"))
-    }
     
     //        static let jonathan = Player(name: "Jonathan Swift", number: "67", position: "4", atBat: "1B")
     func testInningOneJonathanHitsSingle() throws {
-        XCTAssertTrue( Inning.exampleGame[1].top[2].name.contains("Jonathan") )
-        XCTAssertTrue( Inning.exampleGame[1].top[2].atBat.contains("1B") )
+        
+        let duke = Player(name: "Duke Java", number: "33", position: "7", atBat: "1B")
+        let james = Player(name: "James Gosling", number: "4", position: "5", atBat: "2B")
+        let scott = Player(name: "Scott McNealy", number: "37", position: "8", atBat: "K..")
+        let billJoy = Player(name: "Bill Joy", number: "39", position: "9", atBat: "F8")
+        let andy = Player(name: "Andy Bechtolsheim", number: "41", position: "1", atBat: "BB")
+        let larry = Player(name: "Larry Ellison", number: "2", position: "2", atBat: "K")
+       // home team (bottom of innings)
+        let taylor = Player(name: "Taylor Swift", number: "17", position: "8", atBat: "BB")
+        let bill = Player(name: "Bill Swift", number: "18", position: "1", atBat: "K")
+        let jonathan = Player(name: "Jonathan Swift", number: "67", position: "4", atBat: "1B")
+        let bob = Player(name: "Bob Swift", number: "2", position: "2", atBat: "DP")
+       
+   
+       let firstInning = Inning(number: "1",
+                             top: [duke, james, scott, billJoy, andy, larry],
+                             bottom: [taylor, bill, jonathan, bob],
+                             summary: "0 to 0")
+        
+        XCTAssertTrue( firstInning.bottom[2].name.contains("Jonathan") )
+        XCTAssertTrue( firstInning.bottom[2].atBat.contains("1B") )
     }
+    
 }
--- Baseball/Model/Inning.swift
+++ Baseball/Model/Inning.swift
@@ -8,10 +8,10 @@
 import Foundation
 
 public class Inning {
-    var number: String = "1"  // an inning number 1 - 9 typical game length
+    var number: String  // an inning number 1 - 9 typical game length
     var top: [Player]
     var bottom: [Player]
-    var summary: String = "0 to 0"  // the inning's score:  Home to Visitor
+    var summary: String  // the inning's score:  Home to Visitor
     
     init(number: String, top: [Player], bottom: [Player], summary: String) {
         self.number = number
@@ -20,28 +20,44 @@ public class Inning {
         self.summary = summary
     }
     
-    // just so array indexes match names
-    static let zero = Inning(number: "0", top: [], bottom: [], summary: "0 to 0")
-    
-        static let taylor = Player(name: "Taylor Swift", number: "17", position: "8", atBat: "BB")
-        static let bill = Player(name: "Bill Swift", number: "18", position: "1", atBat: "K")
-        static let jonathan = Player(name: "Jonathan Swift", number: "67", position: "4", atBat: "1B")
-        static let bob = Player(name: "Bob Swift", number: "2", position: "2", atBat: "DP")
-        
-        
+    // just so array indexes match names of innings e.g.  index 1 = first inning
+    //static let zero = Inning(number: "0", top: [], bottom: [], summary: "0 to 0")
+
+        // visitors always bat first (top of innings)
         static let duke = Player(name: "Duke Java", number: "33", position: "7", atBat: "1B")
         static let james = Player(name: "James Gosling", number: "4", position: "5", atBat: "2B")
         static let scott = Player(name: "Scott McNealy", number: "37", position: "8", atBat: "K..")
         static let billJoy = Player(name: "Bill Joy", number: "39", position: "9", atBat: "F8")
         static let andy = Player(name: "Andy Bechtolsheim", number: "41", position: "1", atBat: "BB")
-        static let larry = Player(name: "Larry Ellison", number: "2", position: "1", atBat: "K")
+        static let larry = Player(name: "Larry Ellison", number: "2", position: "2", atBat: "K")
+        // home team (bottom of innings)
+        static let taylor = Player(name: "Taylor Swift", number: "17", position: "8", atBat: "BB")
+        static let bill = Player(name: "Bill Swift", number: "18", position: "1", atBat: "K")
+        static let jonathan = Player(name: "Jonathan Swift", number: "67", position: "4", atBat: "1B")
+        static let bob = Player(name: "Bob Swift", number: "8", position: "2", atBat: "DP")
+        
     
     static let first = Inning(number: "1",
-                              top: [taylor, bill, jonathan, bob],
-                              bottom: [duke, james, scott, billJoy, andy, larry],
+                              top: [duke, james, scott, billJoy, andy, larry],
+                              bottom: [taylor, bill, jonathan, bob],
                               summary: "0 to 0")
     
-    static let second = Inning(number: "2", top: [], bottom: [], summary: "0 to 0")
+        static let kay = Player(name: "Kay Swift", number: "87", position: "3", atBat: "BK")
+        static let patrick = Player(name: "Patrick Swift", number: "55", position: "4", atBat: "K")
+        static let todd = Player(name: "Todd Swift", number: "73", position: "5", atBat: "2B")
+        static let connor = Player(name: "Connor Swift", number: "6", position: "9", atBat: "1B")
+        static let ellen = Player(name: "Ellen Swift", number: "88", position: "6", atBat: "DP")
+     
+        static let sun  = Player(name: "Sun Li", number: "62", position: "3", atBat: "1B")
+        static let tzu = Player(name: "Sun Tzu", number: "99", position: "4", atBat: "2B")
+        static let nike = Player(name: "Nike Sun", number: "42", position: "6", atBat: "TP")
+       
+           
+    static let second = Inning(number: "2",
+                               top: [sun, tzu, nike],
+                               bottom: [kay, patrick, todd, connor, ellen],
+                               summary: "0 to 0")
+    
     static let third = Inning(number: "3", top: [], bottom: [], summary: "0 to 0")
     static let fourth = Inning(number: "4", top: [], bottom: [], summary: "0 to 0")
     static let fifth = Inning(number: "5", top: [], bottom: [], summary: "0 to 0")
@@ -51,7 +67,7 @@ public class Inning {
     static let ninth = Inning(number: "9", top: [], bottom: [], summary: "0 to 0")
     
     static var exampleGame: [Inning] =  [
-        zero, first, second, third, fourth, fifth, sixth, seventh, eighth, ninth
+        /* zero, */ first, second, third, fourth, fifth, sixth, seventh, eighth, ninth
     ]
 
     
