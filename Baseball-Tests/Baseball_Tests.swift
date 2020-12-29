0b741aacc3124263e3e78f14fadb30675b1f7db1
davidakoontz <david@koontz.name>
2020-12-28 20:47:53 -0600

    Removed unused files that were part of original (unused) baseball game modeling effort. PlayGenerator, FieldSimulator, Scorecard along with their basic Test cases.

--- Baseball-Tests/Baseball_Tests.swift
+++ Baseball-Tests/Baseball_Tests.swift
@@ -38,49 +38,13 @@ class Baseball_Tests: XCTestCase {
         XCTAssertEqual( play1.batter.name, "Random PlayerName")
     }
     
-    func testBasicSituationAtStart() throws {
-        let sit1 = Situation()
-        
-        XCTAssertTrue( sit1.outs == 0 )
-    }
+   
     
-    func testBasicPlayGenerator() throws {
-        let situation = Situation()
-        let generator = PlayGenerator()
-        // set play's sequence number to 100
-        sequenceNumber = 100
-        let play = generator.startPlay(situation)
-        
-        XCTAssertEqual( play.number, 101 )
-    }
     
-    func testPlayGeneratorRandomPlay() throws {
-        let generator = PlayGenerator()
-        
-        let aPlay = generator.getRandomPlay()
-        
-        XCTAssertEqual( aPlay.description, "random play")  // description of random 
-    }
     
-    func testBasicFieldSimulator() throws {
-        let situation = Situation()
-        let generator = PlayGenerator()
-        let play = generator.startPlay(situation)
-        let HRun = FieldSimulator.isHitAHomeRun( play )
-        
-        XCTAssertFalse( HRun )
-    }
+   
     
-    func testBasicScorecard() throws {
-        let situation = Situation()
-        let generator = PlayGenerator()
-        let play = generator.startPlay(situation)
-        _ = FieldSimulator.isHitAHomeRun( play )
-
-        let card = Scorecard()
-        let desc = card.score( play )
-        
-        XCTAssertEqual(desc, "Hey batter swing")        // a hardcoded Play description from the Generator
-    }
+    
+    
     
 }
