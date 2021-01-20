bf6efbda9ff4e11b84ee3be5ff6d652b55782735
davidakoontz <david@koontz.name>
2021-01-19 21:46:38 -0600

    comment out unused test.
    I've broken the View Inspector test - by changing the view structure with TabView

--- Baseball-ViewTests/Baseball_ViewTests.swift
+++ Baseball-ViewTests/Baseball_ViewTests.swift
@@ -17,16 +17,16 @@ class Baseball_ViewTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+//    func testExample() throws {
+//        // This is an example of a functional test case.
+//        // Use XCTAssert and related functions to verify your tests produce the correct results.
+//    }
+//
+//    func testPerformanceExample() throws {
+//        // This is an example of a performance test case.
+//        measure {
+//            // Put the code you want to measure the time of here.
+//        }
+//    }
 
 }
